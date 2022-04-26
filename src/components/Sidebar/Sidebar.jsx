2700f9a19b77f92defbf8dd425008edd2054489c
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-04-26 10:02:12 +0530

    [feat]:Side menu with links to all pages added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,42 +6,42 @@ import {
   WatchLater,
 } from "@material-ui/icons";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <Link to="/">
+      <NavLink to="/" activeClassName="active" className="link">
         <div className="icon-container">
           <Home className="icon" />
           <h3>Home</h3>
         </div>
-      </Link>
-      <Link to="/playlist">
+      </NavLink>
+      <NavLink to="/playlist" activeClassName="active" className="link">
         <div className="icon-container">
           <PlaylistPlay className="icon" />
           <h3>Playlist</h3>
         </div>
-      </Link>
-      <Link to="/liked-videos">
+      </NavLink>
+      <NavLink to="/liked-videos" activeClassName="active" className="link">
         <div className="icon-container">
           <Favorite className="icon" />
           <h3>Liked Videos</h3>
         </div>
-      </Link>
-      <Link to="/watchlater">
+      </NavLink>
+      <NavLink to="/watchlater" activeClassName="active" className="link">
         <div className="icon-container">
           <WatchLater className="icon" />
           <h3>Watch Later</h3>
         </div>
-      </Link>
-      <Link to="/history">
+      </NavLink>
+      <NavLink to="/history " activeClassName="active" className="link">
         <div className="icon-container">
           <History className="icon" />
           <h3>History</h3>
         </div>
-      </Link>
+      </NavLink>
     </div>
   );
 };
