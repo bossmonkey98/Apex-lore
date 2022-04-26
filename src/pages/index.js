8efcdde694721ffdf3dd90844a3292c8c3cf8851
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-04-26 10:03:44 +0530

    Delete index.js

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Home from "./Home/Home";
-import Playlist from "./Playlist/Playlist";
-import History from "./History/History";
-import Login from "./auth/Login";
-import Signup from "./auth/Signup";
-import Watchlater from "./WatchLater/Watchlater";
-import LikedVideos from "./LikedVideos/LikedVideos";
-
-export { Home, Playlist, History, Login, Signup, Watchlater, LikedVideos };
