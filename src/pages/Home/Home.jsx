277099b2f2ff0dcd77884b34a856b14f6539013e
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-05-07 12:45:42 +0530

    [feat]: filter functionality added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,80 @@
+import { useState } from "react";
+import { Loader, VideoCard } from "../../components";
+import { useVideo } from "../../Context/video-context";
+import { filterByCatagory } from "../../utils";
+
 import "./Home.css";
-import { links } from "./temp";
 
 const Home = () => {
+  const { video } = useVideo();
+  const [catagory, setCatagory] = useState("All");
   return (
-    <div className="home">
-      {links.map((item) => (
-        <div>{item.link}</div>
-      ))}
+    <div style={{ margin: "1rem" }}>
+      <div className="chips">
+        <button
+          className={catagory === "All" ? "chip-btn-visited" : "chip-btn"}
+          onClick={() => {
+            setCatagory("All");
+          }}
+        >
+          All
+        </button>
+        <button
+          className={catagory === "trailers" ? "chip-btn-visited" : "chip-btn"}
+          onClick={() => {
+            setCatagory("trailers");
+          }}
+        >
+          Trailers
+        </button>
+        <button
+          className={
+            catagory === "Outland Stories" ? "chip-btn-visited" : "chip-btn"
+          }
+          onClick={() => {
+            setCatagory("Outland Stories");
+          }}
+        >
+          Stories of Outlands
+        </button>
+        <button
+          className={
+            catagory === "Battle Pass" ? "chip-btn-visited" : "chip-btn"
+          }
+          onClick={() => {
+            setCatagory("Battle Pass");
+          }}
+        >
+          Battle Pass
+        </button>
+        <button
+          className={catagory === "titanfall" ? "chip-btn-visited" : "chip-btn"}
+          onClick={() => {
+            setCatagory("titanfall");
+          }}
+        >
+          Titanfall
+        </button>
+        <button
+          className={catagory === "other" ? "chip-btn-visited" : "chip-btn"}
+          onClick={() => {
+            setCatagory("other");
+          }}
+        >
+          Other
+        </button>
+      </div>
+      <div className="home">
+        {!video ? (
+          <Loader />
+        ) : (
+          <>
+            {filterByCatagory(video, catagory).map((videos) => (
+              <VideoCard key={videos.id} videos={videos} />
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 };
