a317dc8b0cdb410d7ab772de4397556a835066ec
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-05-07 12:41:04 +0530

    Created index file for all components to export for easier readability

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import { Navbar } from "./Navbar/Navbar";
 import { Sidebar } from "./Sidebar/Sidebar";
+import { Loader } from "./loader/Loader";
+import { VideoCard } from "./videocard/VideoCard";
 
-export { Navbar, Sidebar };
+export { Navbar, Sidebar, Loader, VideoCard };
