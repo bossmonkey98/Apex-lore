bda431cb3db6f251df99e740e90f1d4c1018c940
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-05-10 13:04:30 +0530

    [feat]: auth pages added

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,8 +1,70 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../Context/AuthContext";
+import { LogInHandler } from "../../authHandllers/logHandller";
+import "./auth.css";
+
 const Login = () => {
+  const { user, setUser } = useAuth();
+  const [loguser, setLoguser] = useState({ email: "", password: "" });
+  const guestUser = {
+    email: "adarshbalika@gmail.com",
+    password: "adarshBalika123",
+  };
+  const navigator = useNavigate();
   return (
-    <div>
-      Login
-      <button>Signup</button>
+    <div id="log-wrapper">
+      <div id="form-container">
+        <form
+          className="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            LogInHandler(loguser, setUser, navigator);
+          }}
+        >
+          <h1 className="form-heading">APEX LORE / LOGIN</h1>
+          <input
+            className="query"
+            type="text"
+            name="username"
+            placeholder="Enter Email"
+            onChange={(e) => setLoguser({ ...loguser, email: e.target.value })}
+          />
+          <input
+            className="query"
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={(e) => setLoguser({ ...loguser, password: e.target.value })}
+          />
+          <p className="log-cred" style={{ cursor: "context-menu" }}>
+            <span>
+              <input type="radio" />
+              Remember me
+            </span>
+          </p>
+          <div className="log-btn">
+            <button className="btn" type="submit">
+              Login
+            </button>
+            <button
+              className="btn"
+              onClick={(e) => {
+                e.preventDefault();
+                LogInHandler(guestUser, setUser, navigator);
+              }}
+            >
+              Login as Guest
+            </button>
+          </div>
+          <Link
+            to="/signup"
+            style={{ textDecoration: "none", alignSelf: "center" }}
+          >
+            <button className="sign-btn">No Account? Create One</button>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,10 +1,73 @@
-const Signup = () => {
+import React, { useState } from "react";
+import "./auth.css";
+import { Link, useNavigate } from "react-router-dom";
+import { SignUpHandler } from "../../authHandllers/signupHandller";
+
+export default function Signup() {
+  const [SignInUser, setSignInUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
   return (
-    <div>
-      Signup
-      <button>Login</button>
+    <div id="log-wrapper">
+      <div id="form-container">
+        <form className="form">
+          <h1
+            className="form-heading"
+            onSubmit={(e) => {
+              e.preventDefault();
+              SignUpHandler(SignInUser);
+            }}
+          >
+            APEX LORE / SIGN UP
+          </h1>
+          <input
+            className="query"
+            type="text"
+            name="Name"
+            placeholder="Enter Your First Name"
+            onChange={(e) =>
+              setSignInUser({ ...SignInUser, firstName: e.target.val })
+            }
+          />
+          <input
+            className="query"
+            type="text"
+            name="LastName"
+            placeholder="Enter Your Last Name"
+            onChange={(e) =>
+              setSignInUser({ ...SignInUser, lastName: e.target.value })
+            }
+          />
+          <input
+            className="query"
+            type="email"
+            name="email"
+            placeholder="Enter Your E-mail"
+            onChange={(e) =>
+              setSignInUser({ ...SignInUser, email: e.target.value })
+            }
+          />
+          <input
+            className="query"
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={(e) =>
+              setSignInUser({ ...SignInUser, password: e.target.value })
+            }
+          />
+          <button className="btn" style={{ width: "13rem" }}>
+            Sign Up
+          </button>
+          <p>Already have an account?</p>
+          <Link to="/login">
+            <button className="sign-btn">Login</button>
+          </Link>
+        </form>
+      </div>
     </div>
   );
-};
-
-export default Signup;
+}
