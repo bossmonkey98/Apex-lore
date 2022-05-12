c473378737cedd713a5eeba41f4a2156b57d99d2
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-05-12 11:04:43 +0530

    Context for Liked videos created

---
+++ src/Context/LikedVideosContext.js
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { AddToLikedVideos } from "../services/LikedVideos/addToLikedVIdeos";
+import { getLikedVideos } from "../services/LikedVideos/getLikedVideos";
+import { removeFromLikedVideos } from "../services/LikedVideos/removeFromLikedVideos";
+import { useAuth } from "./AuthContext";
+
+const LikedVideosContext = createContext()
+const useLikedVideos = ()=>useContext(LikedVideosContext)
+
+const LikedVideosProvider =({children})=>{
+    const {user} = useAuth()
+    const [LikedVideos , setLikedVideos] = useState([])
+    useEffect(()=>{
+        if(user.isUserLoggedIn){
+            (async()=>{
+                const data = await getLikedVideos(user);
+                if(data){
+                    setLikedVideos(data.likes)
+                }
+            })()}
+            else{
+                setLikedVideos([])
+            }
+    },[user])
+    const AddLikedVideos = async(videos,user)=>{
+        const video = await AddToLikedVideos(videos,user)
+        if(video)
+        setLikedVideos(video.likes)
+    }
+    const RemoveLikedVideos = async(id ,user)=>{
+        const removeVideos = await removeFromLikedVideos(id,user)
+        if(removeVideos)
+        setLikedVideos(removeVideos.likes)
+    }
+    return(
+    <LikedVideosContext.Provider value={{LikedVideos,setLikedVideos,AddLikedVideos,RemoveLikedVideos}}>
+        {children}
+    </LikedVideosContext.Provider>
+)
+}
+
+export {useLikedVideos,LikedVideosProvider}
