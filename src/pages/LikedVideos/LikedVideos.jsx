74a526cb0ee149607319d224433f20af2feb093d
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-05-12 11:06:05 +0530

    [fead]: liked videos page added

--- src/pages/LikedVideos/LikedVideos.jsx
+++ src/pages/LikedVideos/LikedVideos.jsx
@@ -1,5 +1,11 @@
+import { VideoCard } from "../../components/videocard/VideoCard";
+import { useLikedVideos } from "../../Context/LikedVideosContext";
+import './LikedVideos.css'
 const LikedVideos = () => {
-  return <div>Liked Videos</div>;
+  const {LikedVideos} = useLikedVideos()
+  console.log(LikedVideos)
+  return <div className="liked-vids">{LikedVideos!== []?LikedVideos.map((videos)=>(<VideoCard videos={videos}/>)):<>Browse Videos and like them to see your favorite videos</>
+    }</div>;
 };
 
 export default LikedVideos;
