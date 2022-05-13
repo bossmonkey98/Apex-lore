28f5b3bd0becd1163fcfa0333c640520f5784af4
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-05-13 22:46:09 +0530

    [feat] watch later page added

--- src/pages/WatchLater/Watchlater.jsx
+++ src/pages/WatchLater/Watchlater.jsx
@@ -1,5 +1,10 @@
-const Watchlater = () => {
-  return <div>Watchlater</div>;
+import { VideoCard } from "../../components/videocard/VideoCard";
+import { useWatchLater } from "../../Context/watchlater-context";
+
+const WatchLater = () => {
+  const {watchlater} = useWatchLater()
+  return <div className="liked-vids">{watchlater.length !== 0?watchlater.map((videos)=>(<VideoCard videos={videos}/>)):<>No videos found in watchlater</>
+    }</div>;
 };
 
-export default Watchlater;
+export default WatchLater;
