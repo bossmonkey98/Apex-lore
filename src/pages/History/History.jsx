033922d1445c9decd4f18af62c684a5547fed9bf
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-07-02 12:48:37 +0530

    [fix]: dislike and remove history bug fix

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -7,7 +7,7 @@ const History = () => {
   const {user} = useAuth()
   const {history ,clearHistory} = useHistory()
   return <div className="liked-vids" id="history">
-    {history.length === 0 ? <>No history found</> : history.map((videos) => <VideoCard key={videos.id} videos={videos} showbtn={true} />)}
+    {history.length === 0 ? <>No history found</> : history.map((videos) => <VideoCard key={videos.id} videos={videos} showbtn={true} inhistory={true} />)}
   <button className="clr-btn" onClick={()=>clearHistory(user)}>Clear History</button>
   </div>;
 };
--- src/pages/LikedVideos/LikedVideos.jsx
+++ src/pages/LikedVideos/LikedVideos.jsx
@@ -3,7 +3,7 @@ import { useLikedVideos } from "../../Context/LikedVideosContext";
 import './LikedVideos.css'
 const LikedVideos = () => {
   const {LikedVideos} = useLikedVideos()
-  return <div className="liked-vids">{LikedVideos.length !== 0 ? LikedVideos.map((videos) => (<VideoCard videos={videos} showbtn={true} />)):<>Browse Videos and like them to see your favorite videos</>
+  return <div className="liked-vids">{LikedVideos.length !== 0 ? LikedVideos.map((videos) => (<VideoCard videos={videos} showbtn={true} likedVideo={true} />)):<>Browse Videos and like them to see your favorite videos</>
     }</div>;
 };
 
