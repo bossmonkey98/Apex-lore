9addb7b4926bd877a9ce64490b008d861d9987a7
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-08-08 23:30:08 +0530

    minor changes

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -4,26 +4,30 @@ import { useHistory } from "../../Context/History-context";
 import { useAuth } from '../../Context/AuthContext';
 
 const History = () => {
-  const {user} = useAuth()
-  const {history ,clearHistory} = useHistory()
-  
+  const { user } = useAuth()
+  const { history, clearHistory } = useHistory()
+
   return (
-    <div className="liked-vids" id="history">
-      {
-        history.length === 0 ?
-          <>
-            No history found
-          </> :
-          history.map((videos) =>
-            <VideoCard
-              key={videos.id}
-              videos={videos}
-              showbtn={true}
-              inhistory={true}
-              title='remove from history'
-            />)}
-  <button className="clr-btn" onClick={()=>clearHistory(user)}>Clear History</button>
-  </div>
-)};
+    <div id="history">
+      <h1>History</h1>
+      <div className="liked-vids">
+        {
+          history.length === 0 ?
+            <h3>
+              No history found
+            </h3> :
+            history.map((videos) =>
+              <VideoCard
+                key={videos.id}
+                videos={videos}
+                showbtn={true}
+                inhistory={true}
+                title='remove from history'
+              />)}
+        <button className="clr-btn" onClick={() => clearHistory(user)}>Clear History</button>
+      </div>
+    </div>
+  )
+};
 
 export default History;
